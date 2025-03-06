91ea6a9b8a8c8e6eb98943c147c03e060906fab0
walter cruz <cruzherreria1@gmail.com>
2025-03-06 19:18:24 -0300

    modified route backend

--- src/components/ruteBack/vbledeploy.js
+++ src/components/ruteBack/vbledeploy.js
@@ -20,8 +20,8 @@
 // export default host
 
 
-//const rutaBackend="https://backend-app-peluqueria.vercel.app"
-const rutaBackend="backend-app-peluqueria-ns26xe7p8-walcruz198821s-projects.vercel.app"
+const rutaBackend="https://backend-app-peluqueria.vercel.app"
+//const rutaBackend="backend-app-peluqueria-ns26xe7p8-walcruz198821s-projects.vercel.app"
 //const rutaBackend="http://localhost:3002"
 
 
