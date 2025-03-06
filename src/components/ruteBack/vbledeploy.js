aafaa4e315f1a80ad7ded0f118b5c953e3e5fa6c
walter cruz <cruzherreria1@gmail.com>
2025-03-06 18:45:30 -0300

    merge with release

--- src/components/ruteBack/vbledeploy.js
+++ src/components/ruteBack/vbledeploy.js
@@ -21,8 +21,8 @@
 
 
 //const rutaBackend="https://backend-app-peluqueria.vercel.app"
-//const rutaBackend="backend-app-peluqueria-ns26xe7p8-walcruz198821s-projects.vercel.app"
-const rutaBackend="http://localhost:3002"
+const rutaBackend="backend-app-peluqueria-ns26xe7p8-walcruz198821s-projects.vercel.app"
+//const rutaBackend="http://localhost:3002"
 
 
 export default rutaBackend
