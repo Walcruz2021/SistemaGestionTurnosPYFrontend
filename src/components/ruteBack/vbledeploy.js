c8f438a27513ded198f864fc66059d61a18cae64
walter cruz <cruzherreria1@gmail.com>
2025-08-07 15:04:31 -0300

    fix edit turn and edit client

--- src/components/ruteBack/vbledeploy.js
+++ src/components/ruteBack/vbledeploy.js
@@ -19,9 +19,9 @@
 
 // export default host
 
-//const rutaBackend="https://backend-app-peluqueria.vercel.app"
+const rutaBackend="https://backend-app-peluqueria.vercel.app"
 //const rutaBackend="backend-app-peluqueria-ns26xe7p8-walcruz198821s-projects.vercel.app"
-const rutaBackend="http://localhost:3002"
+//const rutaBackend="http://localhost:3002"
 
 
 export default rutaBackend
